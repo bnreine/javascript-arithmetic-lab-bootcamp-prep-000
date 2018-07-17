aea00b86dccb7e97ebca4027e1c82ca12dbd3bda
bnreinecke0209@gmail.com <bnreinecke0209@gmail.com>
2018-07-17 19:37:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,4 +14,19 @@ function divide(a, b) {
   return a/b;
 }
 
+function inc(n) {
+  return n+1;
+}
 
+
+/*
+******************************
+it('inc(n) increments n and returns the result', function() {
+  expect(inc(a)).toEqual(a + 1)
+})
+
+it('dec(n) decrements n and returns the result', function() {
+  expect(dec(a)).toEqual(a - 1)
+})
+********************************
+*/
