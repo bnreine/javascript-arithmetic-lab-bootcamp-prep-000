9efe808376052837ea7ccdc8ddf4cf772f31c80c
bnreinecke0209@gmail.com <bnreinecke0209@gmail.com>
2018-07-17 19:53:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -33,13 +33,5 @@ function makeInt(n) {
 ******************************
 
 
-  it('assumes base 10', function() {
-    expect(makeInt('0x2328')).toEqual(0)
-  })
-
-  it('returns NaN as appropriate', function() {
-    expect(isNaN(makeInt('sldkjflksjf'))).toEqual(true)
-  })
-})
 ********************************
 */
